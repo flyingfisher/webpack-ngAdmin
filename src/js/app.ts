5409e584b665de1118413fba5f33177d5ca3bfbd
yuxiang <yuxiang@yuhongtech.com>
2015-06-30 09:21:24 +0800

    rewrite app.ts fix require.context bug

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,33 +1,45 @@
 /// <reference path="../../typings/app.d.ts" />
 
 angular.module('controllers',[]);
-requireDir("./controller");
+requireController();
 angular.module('services',[]);
-requireDir("./service");
+requireService();
 angular.module('directives',[]);
-requireDir("./directive");
+requireDirective();
 
 var app = angular.module('app', ['ng-admin','controllers','services','directives']);
 
 app.config((NgAdminConfigurationProvider)=> {
     var nga = NgAdminConfigurationProvider;
     // set the main API endpoint for this admin
-    var admin = nga.application('My backend')
-        .baseApiUrl('http://localhost:3000/');
+    var admin = nga.application('test')
+        .baseApiUrl('http://localhost:3000');
 
-    requireDir("./entity",(it)=>{
-        it.configure(nga, admin);
-    })
+    requireEntity(nga, admin);
 
     nga.configure(admin);
 });
 
-function requireDir(path,callback?){
-    var req = require.context(path, false, /\.ts$/);
-    req.keys().forEach((it)=>{
-        if(callback)
-            callback(req(it));
-        else
-            req(it);            
-    });
+require("./app.router.ts");
+
+function requireController(){
+    // require.context is a trick of webpack, this is the only way to use it
+    // 不要合并函数，这里只能这么用，这个函数是webpack解析的，不是运行时解析的。
+    var req = require.context("./controller", false, /\.ts$/);
+    req.keys().forEach((it)=>req(it));
+}
+
+function requireService(){
+    var req = require.context("./service", false, /\.ts$/);
+    req.keys().forEach((it)=>req(it));
+}
+
+function requireDirective(){
+    var req = require.context("./directive", false, /\.ts$/);
+    req.keys().forEach((it)=>req(it));
+}
+
+function requireEntity(nga, admin){
+    var req = require.context("./entity", false, /\.ts$/);
+    req.keys().forEach((it)=>req(it).configure(nga, admin));
 }
